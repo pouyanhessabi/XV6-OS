d6ab7c80e3cf62d9e4bc5322be64da2d16192963
omid mahyar <omid.mahyar78@gmail.com>
2021-02-02 11:21:00 -0800

    change pr_test

--- pr_test.c
+++ pr_test.c
@@ -7,7 +7,7 @@ int main()
     int pr=0;
     for(int i=0;i<30;i++) // loop will run n times (n=5) 
     {   
-        if(i<5):
+        if(i<5)
             pr=6;
         else if (i<10)
         {
@@ -34,15 +34,16 @@ int main()
         if(fork() == 0) 
         { 
             set_priority(getpid(),pr);
-            for (int i = 0; i < count; i++)
+    
+
+            for (int i = 0; i < 10; i++)
             {
-                printf(1,"%d\n",getpid());
-            
+                printf(1," child number :  %d\n",getpid());
             }
-            exit(0); 
+            exit(); 
         } 
     } 
-    for(int i=0;i<5;i++) // loop will run n times (n=5) 
-    wait(NULL); 
-      
+  while(wait() != -1) { }
+
+    sleep(1);
 } 
