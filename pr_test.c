31f573c83cf61905ba0a0fb52ca2e876dbf07b2c
omid mahyar <omid.mahyar78@gmail.com>
2021-02-02 11:04:17 -0800

    start priority test

---
+++ pr_test.c
@@ -0,0 +1,48 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main() 
+{ 
+    int pr=0;
+    for(int i=0;i<30;i++) // loop will run n times (n=5) 
+    {   
+        if(i<5):
+            pr=6;
+        else if (i<10)
+        {
+            pr=5;
+        }
+        else if (i<15)
+        {
+            pr=4;
+        }
+        else if (i<20)
+        {
+            pr=3;
+        }
+        else if (i<25)
+        {
+            pr=2;
+        }
+          else if (i<30)
+        {
+            pr=1;
+        }
+        
+
+        if(fork() == 0) 
+        { 
+            set_priority(getpid(),pr);
+            for (int i = 0; i < count; i++)
+            {
+                printf(1,"%d\n",getpid());
+            
+            }
+            exit(0); 
+        } 
+    } 
+    for(int i=0;i<5;i++) // loop will run n times (n=5) 
+    wait(NULL); 
+      
+} 
