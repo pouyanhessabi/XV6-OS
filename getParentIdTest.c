39ffa9eb3d31345927ec434a00a72fbfdb0e5636
omid mahyar <omid.mahyar78@gmail.com>
2021-02-02 11:03:42 -0800

    fix get parent bugs

--- getParentIdTest.c
+++ getParentIdTest.c
@@ -5,14 +5,12 @@
 int main(){
     int n = fork();
     if (n > 0 ) { 
-    printf(1,"%d",getParentID());
+    printf(1,"%d\n",getParentID());
 
-    /* wait for all child to terminate */
+   
     while(wait() != -1) { }
 
-    /* give time to parent to reach wait clause */
     sleep(1);
-
-    exit();
 }
+ exit();
 }
