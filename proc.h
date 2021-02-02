23ed6bd2e333d6d6f5540093550a68fc028c0d0c
omid mahyar <omid.mahyar78@gmail.com>
2021-02-02 02:51:02 -0800

    fix bug

--- proc.h
+++ proc.h
@@ -50,7 +50,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int priority=3;                 
+  int priority ;              
 };
 
 // Process memory is laid out contiguously, low addresses first:
